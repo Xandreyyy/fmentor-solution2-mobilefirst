2f57fab3b15cfd70c6c14011735502ec698d4443
Xandreyyy <alejandroesteves2005@gmail.com>
2023-10-04 17:38:36 -0300

    added crew.html dynamic menu

--- js/crew.js
+++ js/crew.js
@@ -1,7 +1,12 @@
 const liElements = document.querySelectorAll(".crew-menu .menu-item")
 const commanderItem = document.querySelectorAll(".crew-menu .menu-item")[0]
 
-let activeElement
+const crewRole = document.getElementById("crewRole")
+const crewName = document.getElementById("crewName")
+const crewDesc = document.getElementById("crewDesc")
+const crewBg = document.getElementById("crewImg").src
+
+let activeElement, arrMembers
 
 document.addEventListener("DOMContentLoaded", ()=>{
     activeElement = commanderItem
@@ -9,12 +14,43 @@ document.addEventListener("DOMContentLoaded", ()=>{
     return activeElement
 })
 
+fetch("../data.json").then(response=>{
+    return response.json()
+    .then(jsonFile=>{
+        return jsonFile.crew
+    }).then(allMembers=>{
+        arrMembers = allMembers.map(member=>{
+            return {
+                role: member.role,
+                name: member.name,
+                desc: member.bio,
+                bg: member.images.png
+            }
+        })
+    })
+})
+
 liElements.forEach(el => {
     el.addEventListener("click", ()=>{
+        let dataValue = el.dataset.crew
+        arrMembers.forEach(member=>{
+            if (dataValue === member.role){
+                injectInHTML(member.role, member.name, member.desc, member.bg)
+            }
+        })
+
         if(el !== activeElement){
             activeElement.classList.remove("active")
         }
         activeElement = el
         activeElement.classList.add("active")
+        return dataValue
     })
 })
+
+function injectInHTML(role, name, desc, bg) {
+    crewRole.innerText = `${role}`
+    crewName.innerText = `${name}`
+    crewDesc.innerText = `${desc}`
+    crewBg.innerText = `${bg}`
+}
